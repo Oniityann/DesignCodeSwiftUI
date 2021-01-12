6e015cbc70969d57e93b98f1c17aefe6abbb6fb6
ZhengYinan <zynfab26@hotmail.com>
2021-01-12 14:49:39 +0800

    Initial Commit
    Initial commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by PH on 2020/12/4.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

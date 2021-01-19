2b86888b80422918848f60cb2d22a98aa4a1e5dd
Oniityann <zynfab26@hotmail.com>
2021-01-19 13:53:19 +0800

    Update CourseList.swift
    add geometry reader

--- DesignCode/CourseList.swift
+++ DesignCode/CourseList.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct CourseList: View {
+  @State var show = false
+  @State var show2 = false
+  
     var body: some View {
-      VStack {
-        CourseView()
+      ScrollView {
+        VStack(spacing: 30.0) {
+          CourseView(show: $show)
+          GeometryReader { geometry in
+            CourseView(show: $show2)
+              .offset(y: show2 ? -geometry.frame(in: .global).minY : 0)
+          }
+          .frame(height: show2 ? Screen.height : 280)
+          .frame(maxWidth: show2 ? .infinity : Screen.width)
+        }
+        .frame(width: Screen.width)
       }
     }
 }
@@ -22,7 +34,7 @@ struct CourseList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
-  @State var show = false
+  @Binding var show: Bool
   
   var body: some View {
     ZStack(alignment: .top) {
